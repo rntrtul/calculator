b6abdea8c85a46627b7dae5910717e9cafb73771
Rntrtul <Sistthelord@gmail.com>
2019-06-21 17:53:30 -0400

    can do calculations with buttons if entered in right format

--- script.js
+++ script.js
@@ -26,21 +26,76 @@ function operate(sign, num1, num2){
     }
 }
 
-const screen = document.querySelector(".screen");
+function isOperator(sign){
+    if (sign === "+" || sign === "-" || sign === "*"|| sign === "/"){
+        return true;
+    } else{
+        return false;
+    }
+}
 
-function display(str){
-    if (str === "clear"){
-        screen.textContent = "";
-    }else {
+const asnwer = document.querySelector(".answer");
+
+function screenToOperate(str){
+    var ans;
+    var num1 = "";
+    var num2 = "";
+    var operator = "";
+    var nowChar = "";
+    const length = str.length;
+
+    for (let i = 0; i < length;i++){
+        nowChar = str.charAt(i);
+        console.log(nowChar);
+
+        if (nowChar >= "0" && nowChar <= "9"){
+            console.log("is digit " + nowChar);
+            if (operator === ""){
+                num1 += nowChar;
+            } else{
+                num2 += nowChar;
+            }
+        } else if (isOperator(nowChar)){
+            console.log("is operator " + nowChar);
+            if (operator === ""){
+                operator = nowChar;
+            } else{
+                ans = operate (operator, Number(num1), Number(num2));
+                num1 = ans;
+                num2 = "";
+                operator = nowChar;
+            }            
+        }
+    }
+
+    ans = operate (operator, Number(num1), Number(num2));
+    console.log ("num1: " + num1 + " num2: " + num2);
+    console.log (ans);
+    asnwer.textContent = ans;
+}
+
+const screen = document.querySelector(".screen");
+var screenText = "";
+
+function screenDraw(str){
+    screenText+= str;
+    if (str === "="){
+        screenToOperate (screenText);
+        screen.textContent = " ";
+        screenText= "";
+    } else if (str === "clear"){
+        screen.textContent = " ";
+        screenText= "";
+    } else {
         screen.textContent += str;
     }
-    
 }
+
 
 const btns = document.querySelectorAll("button");
 btns.forEach(element => {
     element.addEventListener('click', (e)=>{
-        display(element.textContent);
+        screenDraw(element.textContent);
     })
 });
 
