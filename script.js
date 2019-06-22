91753b0e472cb6f5b5701198ae8ef80798f67ecf
Rntrtul <Sistthelord@gmail.com>
2019-06-22 14:05:40 -0400

    added decimal and delete button

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function add(num1, num2) {
-    return num1 + num2;
+    return Number(num1) + Number(num2);
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    return Number(num1) - Number(num2);
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    return Math.round((Number(num1) / Number(num2)) * 100) / 100;
 }
 
 function multiply(num1, num2) {
-    return (Math.round((num1 * num2) * 100) / 100);
+    return (Math.round((Number(num1) * Number(num2)) * 100) / 100);
 }
 
 function operate(sign, num1, num2) {
@@ -35,6 +35,7 @@ function isOperator(sign) {
 }
 
 const asnwer = document.querySelector(".answer");
+var decimal = 0;
 
 function screenToOperate(str) {
     var ans;
@@ -47,7 +48,7 @@ function screenToOperate(str) {
     for (let i = 0; i < length; i++) {
         nowChar = str.charAt(i);
 
-        if (nowChar >= "0" && nowChar <= "9") {
+        if (nowChar >= "0" && nowChar <= "9" || nowChar === ".") {
             if (operator === "") {
                 num1 += nowChar;
             } else {
@@ -56,16 +57,19 @@ function screenToOperate(str) {
         } else if (isOperator(nowChar)) {
             if (operator === "") {
                 operator = nowChar;
+                decimal = 0;
             } else {
-                ans = operate(operator, Number(num1), Number(num2));
+                ans = operate(operator, num1, num2);
                 num1 = ans;
                 num2 = "";
                 operator = nowChar;
+                decimal = 0;
             }
         }
     }
 
-    ans = operate(operator, Number(num1), Number(num2));
+    ans = operate(operator, num1, num2);
+    console.log(ans);
     asnwer.textContent = ans;
 }
 
@@ -73,27 +77,43 @@ const screen = document.querySelector(".screen");
 var screenText = "";
 
 function screenDraw(str) {
-    screenText += str;
-    if (str === "=") {
-        screenToOperate(screenText);
-        screen.textContent = " ";
-        screenText = "";
-    } else if (str === "AC") {
-        screen.textContent = " ";
-        screenText = "";
+    if (str == "Del") {
+        var shorter = screen.textContent.substring(0, screen.textContent.length - 1);
+        screen.textContent = shorter;
     } else {
-        if (isOperator(str)) {
-            screen.textContent += " " + str + " ";
+        screenText += str;
+        if (str === "=") {
+            screenToOperate(screenText);
+            screen.textContent = " ";
+            screenText = "";
+        } else if (str === "AC") {
+            screen.textContent = " ";
+            screenText = "";
+            asnwer.textContent = "";
         } else {
-            screen.textContent += str;
+            if (isOperator(str)) {
+                screen.textContent += " " + str + " ";
+                decimal = 0;
+            } else {
+                screen.textContent += str;
+            }
         }
     }
 }
 
-
 const btns = document.querySelectorAll("button");
+
 btns.forEach(element => {
-    element.addEventListener('click', (e) => {
-        screenDraw(element.textContent);
-    })
+    if (element.textContent === ".") {
+        element.addEventListener('click', (e) => {
+            if (decimal === 0) {
+                decimal = 1;
+                screenDraw(element.textContent);
+            }
+        });
+    } else {
+        element.addEventListener('click', (e) => {
+            screenDraw(element.textContent);
+        });
+    }
 });
